b534b8868054bfa18dda6d73c0e783e67d045bce
pfeairheller <pfeairheller@gmail.com>
2023-02-04 18:24:44 -0800

    Some structure for the project, an initial command for launching an agent and an Authenticater for signing responses and verifying signed requests.

---
+++ tests/core/test_authing.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8 -*-
+"""
+SIGNIFY
+signify.core.httping module
+
+Testing httping utils
+"""
+import falcon
+import pytest
+from falcon import testing
+from hio.help import Hict
+from keri import kering
+from keri.app import habbing
+from keri.core import parsing, eventing
+
+from signify.core import authing
+
+
+def test_authenticater(mockHelpingNowUTC):
+    salt = b'0123456789abcdef'
+    with habbing.openHab(name="controller", salt=salt, temp=True) as (controllerHby, controller), \
+            habbing.openHab(name="agent", salt=salt, temp=True) as (agentHby, agent):
+
+        # Create authenticater with Agent and controllers AID
+        authn = authing.Authenticater(agent=agent, caid=controller.pre)
+        signer = authing.Authenticater(agent=controller, caid=agent.pre)
+
+        rep = falcon.Response()
+        with pytest.raises(kering.AuthNError):  # Should fail if Agent hasn't resolved controller's KEL
+            authn.verify(rep)
+
+        agentKev = eventing.Kevery(db=agent.db, lax=True, local=False)
+        icp = controller.makeOwnInception()
+        parsing.Parser().parse(ims=bytearray(icp), kvy=agentKev)
+
+        assert controller.pre in agent.kevers
+
+        headers = Hict([
+            ("Content-Type", "application/json"),
+            ("Content-Length", "256"),
+            ("Connection", "close"),
+            ("Signify-Resource", "EWJkQCFvKuyxZi582yJPb0wcwuW3VXmFNuvbQuBpgmIs"),
+            ("Signify-Timestamp", "2022-09-24T00:05:48.196795+00:00"),
+        ])
+
+        headers = signer.sign(headers, method="POST", path="/boot")
+        assert dict(headers) == {'Connection': 'close',
+                                 'Content-Length': '256',
+                                 'Content-Type': 'application/json',
+                                 'Signature': ('indexed="?0";signify="bipHos8-XTOzLq0He4tz8mIeZGq4h5WdIndNVCSX2H5eYCqwY'
+                                               'OQT7EysiMkgp0HwYBIgmg7wuTQgtJKJ__EBCA=="'),
+                                 'Signature-Input': ('signify=("signify-resource" "@method" "@path" '
+                                                     '"signify-timestamp");created=1609459200;keyid="EAM6vT0VYoaEWxRTgr'
+                                                     '24g0nZHmPSUBgs19WB43zEKHnz";alg="ed25519"'),
+                                 'Signify-Resource': 'EWJkQCFvKuyxZi582yJPb0wcwuW3VXmFNuvbQuBpgmIs',
+                                 'Signify-Timestamp': '2022-09-24T00:05:48.196795+00:00'}
+
+        req = testing.create_req(method="POST", path="/boot", headers=dict(headers))
+        assert authn.verify(req)
