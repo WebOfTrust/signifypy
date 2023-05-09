cb02dd55eb3282c8c44ef5a333833540747edc62
pfeairheller <pfeairheller@gmail.com>
2023-05-08 17:19:43 -0700

    Updating SaltyKeeper to use a new salt for the key-chain for every AID.
    Added proper unit tests for SaltyKeeper
    
    Added proper documentation for SaltyKeeper

--- src/signify/core/keeping.py
+++ src/signify/core/keeping.py
@@ -114,17 +114,51 @@ class BaseKeeper:
 
 
 class SaltyKeeper(BaseKeeper):
+    """
+    Keeper class for managing keys for an AID that uses a hierarchical deterministic key chain with a salt
+    per AID.  The passcode is used as an encryption key to encrypt and store the AID's salt on the server.
+    This class can either be instantiated with an encrypted salt or None which will create a random salt for this AID.
+
+    """
     stem = "signify:aid"
 
     def __init__(self, salter, pidx, kidx=0, tier=Tiers.low, transferable=False, stem=None,
-                 code=MtrDex.Ed25519_Seed, count=1, icodes=None,
-                 ncode=MtrDex.Ed25519_Seed, ncount=1, ncodes=None, dcode=MtrDex.Blake3_256):
+                 code=MtrDex.Ed25519_Seed, count=1, icodes=None, ncode=MtrDex.Ed25519_Seed,
+                 ncount=1, ncodes=None, dcode=MtrDex.Blake3_256, sxlt=None):
+        """
+        Create an instance of a SaltyKeeper for managing keys for a single AID.  This can be created from
+        data saved externally to recreate keys at a given point in time or with values for a new AID.  The sxlt
+        parameter can contain an existing encrypted salt to use for the HDK algorithm for this key chain or None
+        to create a new random one for a new AID
 
-        self.salter = salter
+
+        Parameters:
+            salter (Salter): encryption salter used for encrypting the AID salt.  Typically user passcode
+            pidx (int):  prefix relative index for this AID in a Habery of AIDs
+            kidx (int): key index for the current state of the key chain
+            tier (Tiers): secret derivation security tier
+            transferable (bool): True if the AID for this keeper can establish new keys
+            stem (str): prefix for the path generated for key creation
+            code (str): derivation code for signing key creation
+            count (int): number of signing keys
+            icodes (list): alternate to code and count to be specific about key codes
+            ncode (str): derivation code for rotation key creation
+            ncount (int): number of rotation keys
+            ncodes (list): alternate to ncode and ncount to be specific about rotation key codes
+            dcode (str): derivation code for hashing algorithm for next key digests
+            sxlt (str): qualified base64 of cipher of AID salt.
+        """
+
         if not icodes:  # if not codes make list len count of same code
             icodes = [code] * count
         if not ncodes:
             ncodes = [ncode] * ncount
+
+        # Salter is the entered passcode and used for enc/dec of salts for each AID
+        signer = salter.signer(transferable=False)
+        self.aeid = signer.verfer.qb64
+        self.encrypter = coring.Encrypter(verkey=self.aeid)
+        self.decrypter = coring.Decrypter(seed=signer.qb64)
 
         self.tier = tier
         self.icodes = icodes
@@ -135,10 +169,20 @@ class SaltyKeeper(BaseKeeper):
         self.transferable = transferable
         stem = stem if stem is not None else self.stem
 
-        self.creator = SaltyCreator(salt=salter.qb64, stem=stem, tier=tier)
+        # Sxlt is encrypted salt for this AID or None if incepting
+        if sxlt is None:
+            self.creator = SaltyCreator(stem=stem, tier=tier)
+            self.sxlt = self.encrypter.encrypt(self.creator.salt).qb64
+        else:
+            self.sxlt = sxlt
+            ciph = coring.Cipher(qb64=self.sxlt)
+            self.creator = SaltyCreator(self.decrypter.decrypt(cipher=ciph).qb64, stem=stem, tier=tier)
 
     def params(self):
+        """ Get AID parameters to store externally """
+
         return dict(
+            sxlt=self.sxlt,
             pidx=self.pidx,
             kidx=self.kidx,
             stem=self.stem,
@@ -150,6 +194,16 @@ class SaltyKeeper(BaseKeeper):
         )
 
     def incept(self, transferable):
+        """ Create verfers and digers for inception event for AID represented by this Keeper
+
+        Args:
+            transferable (bool): True if the AID for this keeper can establish new keys
+
+        Returns:
+            verfers(list): qualified base64 of signing public keys
+            digers(list): qualified base64 of hash of rotation public keys
+
+        """
         self.transferable = transferable
         self.kidx = 0
 
@@ -164,6 +218,17 @@ class SaltyKeeper(BaseKeeper):
         return verfers, digers
 
     def rotate(self, ncodes, transferable, **_):
+        """ Rotate and return verfers and digers for next rotation event for AID represented by this Keeper
+
+        Args:
+            ncodes (list):
+            transferable (bool): derivation codes for rotation key creation
+
+        Returns:
+            verfers(list): qualified base64 of signing public keys
+            digers(list): qualified base64 of hash of rotation public keys
+
+        """
         signers = self.creator.create(codes=self.ncodes, pidx=self.pidx, kidx=self.kidx + len(self.icodes),
                                       transferable=self.transferable)
         verfers = [signer.verfer.qb64 for signer in signers]
@@ -176,6 +241,18 @@ class SaltyKeeper(BaseKeeper):
         return verfers, digers
 
     def sign(self, ser, indexed=True, indices=None, ondices=None):
+        """ Sign provided data using the current signing keys for AID
+
+        Args:
+            ser (bytes): data to sign
+            indexed (bool): True indicates the signatures are to be indexed signatures (indexed code)
+            indices (list): specified signing indicies for each signature generated
+            ondices (list): specified rotation indicies for each signature generated
+
+        Returns:
+            list: qualified b64 CESR encoded signatures
+
+        """
         signers = self.creator.create(codes=self.icodes, pidx=self.pidx, kidx=self.kidx,
                                       transferable=self.transferable)
 
---
+++ tests/core/test_keeping.py
@@ -0,0 +1,85 @@
+# -*- encoding: utf-8 -*-
+"""
+SIGNIFY
+signify.core.keeping module
+
+Testing authentication
+"""
+
+from keri.core import coring
+
+from signify.core import keeping
+
+
+def test_salty_keeper():
+    bran = b'0123456789abcdefghijk'
+    bran = coring.MtrDex.Salt_128.encode("utf-8") + b'A' + bran
+    salter = coring.Salter(qb64=bran)
+
+    # Create a Salter without specifying the AIDs salt, let it create one randomly
+    keeper = keeping.SaltyKeeper(salter=salter, pidx=0, kidx=0)
+    verfers, ndigs = keeper.incept(transferable=True)
+    assert len(verfers) == 1
+    assert coring.Matter(qb64=verfers[0]).code == coring.MtrDex.Ed25519
+    assert verfers[0] != "DPmhSfdhCPxr3EqjxzEtF8TVy0YX7ATo0Uc8oo2cnmY9"  # this seems dumb
+    assert len(ndigs) == 1
+    assert coring.Matter(qb64=ndigs[0]).code == coring.MtrDex.Blake3_256
+    assert ndigs[0] != "EAORnRtObOgNiOlMolji-KijC_isa3lRDpHCsol79cOc"  # this seems dumb
+
+    # Now lets use the same salt as the passcode (not advisable) so we have some determinism
+    signer = salter.signer(transferable=False)
+    encrypter = coring.Encrypter(verkey=signer.verfer.qb64)
+    sxlt = encrypter.encrypt(matter=coring.Matter(qb64b=bran)).qb64
+
+    keeper = keeping.SaltyKeeper(salter=salter, pidx=0, kidx=0, sxlt=sxlt)
+    verfers, ndigs = keeper.incept(transferable=True)
+    assert len(verfers) == 1
+    assert verfers[0] == "DPmhSfdhCPxr3EqjxzEtF8TVy0YX7ATo0Uc8oo2cnmY9"
+    assert len(ndigs) == 1
+    assert ndigs[0] == "EAORnRtObOgNiOlMolji-KijC_isa3lRDpHCsol79cOc"
+
+    params = keeper.params()
+    assert params['pidx'] == 0
+    assert params['kidx'] == 0
+    assert params['dcode'] == 'E'
+    assert params['icodes'] == ['A']
+    assert params['ncodes'] == ['A']
+    assert params['stem'] == 'signify:aid'
+    assert params['transferable'] is True
+
+    ser = b'I like salty keys that have a salt per AID not tied to the passcode'
+    sigs = keeper.sign(ser=ser, indexed=True)
+    assert len(sigs) == 1
+    assert sigs[0] == 'AAA7tNQo83P-4wvFcnROAn_YPmcVo1ywESOwhXs0FBwQroeeSrRFjr0gK20lu8ZOsg3fKxbJzXlzZbYKfryFv4kC'
+
+    verfers, ndigs = keeper.rotate(ncodes=[coring.MtrDex.Ed25519_Seed], transferable=True)
+    assert len(verfers) == 1
+    assert verfers[0] == "DHgomzINlGJHr-XP3sv2ZcR9QsIEYS3LJhs4KRaZYKly"
+    assert len(ndigs) == 1
+    assert ndigs[0] == "EJMovBlrBuD6BVeUsGSxLjczbLEbZU9YnTSud9K4nVzk"
+    assert keeper.pidx == 0
+    assert keeper.kidx == 1
+
+    params = keeper.params()
+    assert params['pidx'] == 0
+    assert params['kidx'] == 1
+    assert params['dcode'] == 'E'
+    assert params['icodes'] == ['A']
+    assert params['ncodes'] == ['A']
+    assert params['stem'] == 'signify:aid'
+    assert params['transferable'] is True
+
+    # Sign something after the rotation, get a different signature
+    sigs = keeper.sign(ser=ser, indexed=True)
+    assert len(sigs) == 1
+    assert sigs[0] == 'AAAMgsRDpbMrLGJt4RX6uDMA1eCJ7eJ6tKIuLQzBY-lh9fGWe-A3v8_dDUzZDPzuokEnPfe_u7QBWNeEV6DkaHMN'
+
+    # Recreate the Salter at this kidx and get the same signature
+    keeper = keeping.SaltyKeeper(salter=salter, pidx=0, kidx=1, sxlt=sxlt)
+    sigs = keeper.sign(ser=ser, indexed=True)
+    assert len(sigs) == 1
+    assert sigs[0] == 'AAAMgsRDpbMrLGJt4RX6uDMA1eCJ7eJ6tKIuLQzBY-lh9fGWe-A3v8_dDUzZDPzuokEnPfe_u7QBWNeEV6DkaHMN'
+
+
+
+
