6f88ef9c054cb438fee5d1bf0afa32a9648b704c
Kevin Griffin <griffin.kev@gmail.com>
2023-08-22 16:29:02 +0000

    aligns tests with Kevins latest whim (#69)

--- tests/app/test_notifying.py
+++ tests/app/test_notifying.py
@@ -6,7 +6,7 @@ signify.app.test_clienting module
 Testing notifying with unit tests
 """
 
-from mockito import mock,  when, unstub
+from mockito import mock, unstub, expect, verifyNoUnwantedInteractions
 
 
 def test_notification_list():
@@ -14,12 +14,12 @@ def test_notification_list():
     mock_client = mock(spec=SignifyClient)
 
     from signify.app.notifying import Notifications
-    notes = Notifications(client=mock_client)
+    notes = Notifications(client=mock_client) # type: ignore
 
     from requests import Response
-    mock_response = mock({'headers': {'content-range': 'notes 0-10/20'}}, spec=Response)
-    when(mock_client).get('/notifications', headers=dict(Range=f"notes={0}-{24}")).thenReturn(mock_response)
-    when(mock_response).json().thenReturn(
+    mock_response = mock({'headers': {'content-range': 'notes 0-10/20'}}, spec=Response, strict=True)
+    expect(mock_client, times=1).get('/notifications', headers=dict(Range=f"notes={0}-{24}")).thenReturn(mock_response)
+    expect(mock_response, times=1).json().thenReturn(
         ['note1', 'note2']
     )
 
@@ -29,46 +29,53 @@ def test_notification_list():
     assert out['total'] == 20
     assert out['notes'] == ['note1', 'note2']
 
+    verifyNoUnwantedInteractions()
     unstub()
 
 
 def test_noticiation_mark_as_read():
     from signify.app.clienting import SignifyClient
-    mock_client = mock(spec=SignifyClient)
+    mock_client = mock(spec=SignifyClient, strict=True)
 
     from signify.app.notifying import Notifications
-    notes = Notifications(client=mock_client)
+    notes = Notifications(client=mock_client) # type: ignore
 
     from requests import Response
-    mock_response = mock({'status': 202}, spec=Response)
-    when(mock_client).put('/notifications/ABC123', json={}).thenReturn(mock_response)
+    mock_response = mock({'status': 202}, spec=Response, strict=True)
+    expect(mock_client, times=1).put('/notifications/ABC123', json={}).thenReturn(mock_response)
 
     out = notes.markAsRead(nid="ABC123")
     assert out is True
 
-    mock_response = mock({'status': 404}, spec=Response)
-    when(mock_client).put('/notifications/DEF456', json={}).thenReturn(mock_response)
+    mock_response = mock({'status': 404}, spec=Response, strict=True)
+    expect(mock_client, times=1).put('/notifications/DEF456', json={}).thenReturn(mock_response)
 
     out = notes.markAsRead(nid="DEF456")
     assert out is False
 
+    verifyNoUnwantedInteractions()
+    unstub()
+
 
 def test_noticiation_delete():
     from signify.app.clienting import SignifyClient
-    mock_client = mock(spec=SignifyClient)
+    mock_client = mock(spec=SignifyClient, strict=True)
 
     from signify.app.notifying import Notifications
-    notes = Notifications(client=mock_client)
+    notes = Notifications(client=mock_client) # type: ignore
 
     from requests import Response
-    mock_response = mock({'status': 202}, spec=Response)
-    when(mock_client).delete(path='/notifications/ABC123').thenReturn(mock_response)
+    mock_response = mock({'status': 202}, spec=Response, strict=True)
+    expect(mock_client, times=1).delete(path='/notifications/ABC123').thenReturn(mock_response)
 
     out = notes.delete(nid="ABC123")
     assert out is True
 
-    mock_response = mock({'status': 404}, spec=Response)
-    when(mock_client).delete(path='/notifications/DEF456').thenReturn(mock_response)
+    mock_response = mock({'status': 404}, spec=Response, strict=True)
+    expect(mock_client, times=1).delete(path='/notifications/DEF456').thenReturn(mock_response)
 
     out = notes.delete(nid="DEF456")
     assert out is False
+
+    verifyNoUnwantedInteractions()
+    unstub()
