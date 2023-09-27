16128596908ade7cc3a6c0cc9012c71feaad33b7
pfeairheller <pfeairheller@gmail.com>
2023-09-26 19:48:05 -0700

    Test coverage for new credentialing APIs

--- tests/app/test_credentialing.py
+++ tests/app/test_credentialing.py
@@ -5,8 +5,11 @@ signify.app.test_credentialing module
 
 Testing credentialing with unit tests
 """
-
+from keri.vdr import eventing as veventing
+from keri.core import eventing
 from mockito import mock, unstub, verify, verifyNoUnwantedInteractions, expect, ANY
+
+from signify.app import credentialing
 
 
 def test_registries():
@@ -36,7 +39,26 @@ def test_registries():
 
     from signify.app.credentialing import Registries
 
-    Registries(client=mock_client).create(hab=mock_hab, registryName=regName)
+    registries = Registries(client=mock_client)
+    registries.create(hab=mock_hab, registryName=regName)
+
+    expect(mock_client, times=1).get(f"/identifiers/{name}/registries/{regName}").thenReturn(mock_response)
+    registries.get(name="aid1", registryName=regName)
+
+    pre = "ELI7pg979AdhmvrjDeam2eAO2SR5niCgnjAJXJHtJose"
+    dig = "EOgQvKz8ziRn7FdR_ebwK9BkaVOnGeXQOJ87N6hMLrK0"
+    nonce = "ACb_3pGwW3uIjtOg4zRQ66I-SggMcmoyju_uCzuSvgG4"
+    serder = veventing.incept(pre=pre, nonce=nonce)
+    anc = eventing.interact(pre=pre, dig=dig)
+
+    msg = Registries.serialize(serder, anc)
+    assert msg == (b'{"v":"KERI10JSON00010f_","t":"vcp","d":"EGaypC6sODRFyIuhdFzzFmBU'
+                   b'4Xe5SNprALGbltnyHYSz","i":"EGaypC6sODRFyIuhdFzzFmBU4Xe5SNprALGbl'
+                   b'tnyHYSz","ii":"ELI7pg979AdhmvrjDeam2eAO2SR5niCgnjAJXJHtJose","s"'
+                   b':"0","c":[],"bt":"0","b":[],"n":"ACb_3pGwW3uIjtOg4zRQ66I-SggMcmo'
+                   b'yju_uCzuSvgG4"}-VAS-GAB0AAAAAAAAAAAAAAAAAAAAAABENns5-voIbnRMADUO'
+                   b'so7HDiQ9ZS_AfU8BfgGLHEW54H1')
+
     unstub()
 
 
@@ -75,4 +97,40 @@ def test_credentials_query():
     assert out == 'things I found'
 
     verifyNoUnwantedInteractions()
+    unstub()
+
+
+def test_ipex():
+    from signify.app.clienting import SignifyClient
+    mock_client = mock(spec=SignifyClient, strict=True)
+
+    from signify.peer.exchanging import Exchanges
+    mock_excs = mock(spec=Exchanges, strict=True)
+
+    dt = "2023-09-25T16:01:37.000000+00:00"
+    mock_hab = {'prefix': 'a_prefix', 'name': 'aid1', 'state': {'s': '1', 'd': "ABCDEFG"}}
+    mock_acdc = {}
+    mock_iss = {}
+    mock_anc = {}
+    mock_grant = {}
+    mock_gsigs = []
+    mock_end = b''
+    expect(mock_client, times=1).exchanges().thenReturn(mock_excs)
+    expect(mock_excs).createExchangeMessage(sender=mock_hab, route="/ipex/grant",
+                                            payload={'m': 'this is a test',
+                                                     'i': 'ELI7pg979AdhmvrjDeam2eAO2SR5niCgnjAJXJHtJose'},
+                                            embeds={'acdc': {}, 'iss': {}, 'anc': {}}, dt=dt).thenReturn((mock_grant,
+                                                                                                         mock_gsigs,
+                                                                                                         mock_end))
+
+    ipex = credentialing.Ipex(mock_client)
+    recp = "ELI7pg979AdhmvrjDeam2eAO2SR5niCgnjAJXJHtJose"
+
+    grant, gsigs, end = ipex.grant(hab=mock_hab, recp=recp, message="this is a test", acdc=mock_acdc, iss=mock_iss,
+                                   anc=mock_anc, dt=dt)
+
+    assert grant == mock_grant
+    assert gsigs == mock_gsigs
+    assert end == mock_end
+
     unstub()
