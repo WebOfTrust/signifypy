b423c298f125f35361ceeccde1cecb2fa088ae04
Kevin Griffin <griffin.kev@gmail.com>
2023-10-30 09:27:23 -0400

    removed KERIA dependency (#90)
    Signed-off-by: Kevin Griffin <griffin.kev@gmail.com>

--- setup.py
+++ setup.py
@@ -66,7 +66,6 @@ setup(
     python_requires='>=3.10.4',
     install_requires=[
         'keri @ git+https://git@github.com/weboftrust/keripy.git@development',
-        'keria>=0.0.1',
         'multicommand>=1.0.0',
         'requests>=2.28',
         'http_sfv>=0.9.8',
